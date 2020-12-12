669d27e82a8652bb0c5ad4db946f5c0b1ab5f072
RustemSult <sultanov.rustem@gmail.com>
2020-12-13 03:25:47 +0500

    0.0.3

--- update.sh
+++ update.sh
@@ -1,3 +1,5 @@
+#!/bin/bash
+# Test script2
 cat <<EOF > /home/test/index.html
 <html>
 <h2>This page is used to test the proper operation of the Apache HTTP server: Upgrade for myapp</h2><br>Build by Shell Script!
