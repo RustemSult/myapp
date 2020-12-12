4f3f4ccf127f40622c277672f45746f9e163c1ce
RustemSult <sultanov.rustem@gmail.com>
2020-12-13 02:39:10 +0500

    v0.0.1

--- update.sh
+++ update.sh
@@ -1,4 +1,4 @@
-cat <<EOF > /opt/myapp/index.html
+cat <<EOF > /home/test/myapp/index.html
 <html>
 <h2>This page is used to test the proper operation of the Apache HTTP server: Upgrade index.html for myapp</h2><br>Build by Shell Script!
 </html>
